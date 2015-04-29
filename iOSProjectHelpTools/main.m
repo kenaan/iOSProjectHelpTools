37dfd4242d38031d72ea87824ff568421ef6e47b
“000000jerry” <761797477@qq.com>
2015-04-29 16:55:16 +0800

    Initial Commit

---
+++ iOSProjectHelpTools/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOSProjectHelpTools
+//
+//  Created by 刘杰cjs on 15/4/29.
+//  Copyright (c) 2015年 com.cjs.lj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
